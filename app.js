1835409931eeac7492df5e5cfc4e8ce44f30aabf
Martín Chavez <cmchavez@applaudostudios..com>
2023-07-03 12:05:46 -0300

    forecast.js and geocode.js were created to separete functionalities

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-  const request = require('postman-request')
+const forecast = require("./utils/forecast.js");
+const geocode = require("./utils/geocode.js");
 
-  const location = '';
-  const token = 'pk.eyJ1IjoiY2NjbW1tY2NjIiwiYSI6ImNqZHpkNmg0dDMza3gzMnQzcWJvdDgzNWQifQ.4IQc3LFSoolZSPqGlNE7pw'
-  const urlMap = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${token}&limit=1`
+if(process.argv[2] === 'clima' && process.argv.length > 3) {
+  const location = process.argv[3]
+  //console.log('location', location)
   
-  request({url: urlMap, json: true}, (error, response, body) => {
-    console.log(response)
-    if(error) {
-      console.log("Unable to connect to the server")
-    } else if(response.body.features.length === 0) {
-      console.log(response.error)
-    } else {
-      const features = response.body
-      const [long, lat] = features.features[0].center
-      console.log(lat, long)
+  geocode(location, (error, {long, lat}) => {
+    if (error) {
+      console.log(error);
+      return;
     }
-  })
+  
+    forecast(long, lat, (error, {location, temperature, rain}) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+  
+      console.log(
+      `${location}: 
+        It's currently ${temperature} degrees out. 
+        There is ${rain}% chance to rain`);
+    });
+  });
+}
 
-  const key = '4bf77eb784fd6b762d89b38f3a598a29';
-  const url = `http://api.weatherstack.com/current?access_key=${key}&query=${location}`;
-
-  // request(url, (error, response, body) => {
-  //   //console.log(JSON.parse(response.body))
-  //   const data = JSON.parse(response.body);
-  //   const temp = data.current.temperature;
-  //   const rain = data.current.precip;
-  //   console.log(`It's currently ${temp} degrees out. There is ${rain}% chance to rain`)
-  // })
